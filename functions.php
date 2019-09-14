deb9967e996ed154dbf08502ab825ee852f8469c
Sk Md Morshed <skmorshed75@gmail.com>
2019-09-14 13:29:38 +0600

    Counter number 4.3

--- functions.php
+++ functions.php
@@ -17,8 +17,18 @@ function launcher_assets(){
 	wp_enqueue_script("easing-jquery-js", get_theme_file_uri("/assets/js/jquery.easing.1.3.js"),array("jquery"),"0.01", true);
 	wp_enqueue_script("bootstrap-jquery-js",get_theme_file_uri("/assets/js/bootstrap.min.js"),array("jquery"), "0.01", true);
 	wp_enqueue_script("waypoints-jquery-js",get_theme_file_uri("/assets/js/jquery.waypoints.min.js"), array("jquery"), "0.01", true);
-	wp_enqueue_script("countdown-jquery-js", get_theme_file_uri("/assets/js/simplyCountdown.js"), array('jquery'), "0.01", true);
-	wp_enqueue_script("main-jquery-js", get_theme_file_uri("/assets/js/main.js"), array('jquery'), "0.01",  true);
+	wp_enqueue_script("countdown-jquery-js", get_theme_file_uri("/assets/js/simplyCountdown.js"), array('jquery'), time(), true);
+	wp_enqueue_script("main-jquery-js", get_theme_file_uri("/assets/js/main.js"), array('jquery'), time(),  true);
+	
+	//class 4.3
+	$launcher_year = get_post_meta(get_the_id(),"year", true);
+	$launcher_month = get_post_meta(get_the_id(),"month", true);
+	$launcher_day = get_post_meta(get_the_id(),"day", true);
+	wp_localize_script("main-jquery-js", "datedata", array(
+		'year'	=> $launcher_year,
+		'month'	=> $launcher_month,
+		'day'	=> $launcher_day,
+	));
 }
 add_action("wp_enqueue_scripts", "launcher_assets");
 
